90e3c180761a21e0ee24d5fe1ad3d6b3d4b36ca2
Skyrimwell <skyrimwell@gmail.com>
2021-04-17 14:06:02 +0300

    AHUET' RABOTAET(Eblya s BD)

---
+++ src/main/java/app/Application/Configuration/Config.java
@@ -0,0 +1,9 @@
+package app.Application.Configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@Configuration
+@EnableJpaRepositories(basePackages = {"app.Application"})
+public class Config {
+}
