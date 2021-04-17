c58a6c304d926808df32c564ad72abb0aa874d7e
Skyrimwell <skyrimwell@gmail.com>
2021-04-17 14:31:49 +0300

    added ebanyi logging

--- src/main/java/app/Application/Services/PhoneService.java
+++ src/main/java/app/Application/Services/PhoneService.java
@@ -2,13 +2,24 @@ package app.Application.Services;
 
 import app.Application.Classes.Phone;
 import app.Application.Interfaces.PhoneRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 import java.util.List;
 import java.util.UUID;
 
+
 @Service
+@Slf4j
 public class PhoneService {
     @Autowired
     private final PhoneRepository phoneRepository;
